8cd38fd73a43c2ed1ed5c369a56b3f6378360cc2
Hossam Elbahrawy <hossamelbahrawy52@gmail.com>
2019-05-23 03:56:43 +0200

    added quantity, remove and totalPrice function

--- src/Components/ShoppingCart/ShoppingCart.js
+++ src/Components/ShoppingCart/ShoppingCart.js
@@ -13,82 +13,150 @@ import {
 export default class ShoppingCart extends Component {
   state = {
     products: [
-      { productName: 'Cool Car', price: 50, quantity: 1 },
-      { productName: 'Cool Car', price: 150, quantity: 2 },
-      { productName: 'Cool Car', price: 500, quantity: 3 }
+      {
+        id: '1',
+        productName: 'Cool Car',
+        price: 50,
+        quantity: 1,
+        stockQuantity: 10
+      },
+      {
+        id: '2',
+        productName: 'Cool Car',
+        price: 150,
+        quantity: 2,
+        stockQuantity: 10
+      },
+      {
+        id: '3',
+        productName: 'Cool Car',
+        price: 500,
+        quantity: 3,
+        stockQuantity: 10
+      }
     ],
     totalPrice: 1000
   };
+  componentDidMount() {
+    // this.getDataHandler();
+    this.calcTotalPrice();
+  }
+  getDataHandler = async () => {
+    axios
+      .get('http://localhost:8000/api/cart/userCart/', {
+        headers: { 'x-auth-token': localStorage.getItem('token') }
+      })
+      .then(async res => {
+        axios
+          .post('http://localhost:8000/api/products/list', { ids: res.data })
+          .then(res => {
+            console.log(res.data);
+            this.setState({ products: res.data });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      })
+      .catch(e => {
+        console.log('err', e);
+      });
+  };
+
+  checkoutHandler = () => {
+    //Checkout
+  };
+
+  removeHandler = id => {
+    const filtered = this.state.products.filter(function(value, index, arr) {
+      return value.id !== id;
+    });
+    this.setState({ products: filtered }, () => {
+      this.calcTotalPrice();
+    });
+  };
+
+  decrementHandler = id => {
+    const { products } = this.state;
+    const productIndex = products.findIndex(product => product.id === id);
+
+    if (products[productIndex].quantity > 0) {
+      products[productIndex].quantity -= 1;
+    }
+    this.setState({ products: products });
+    this.calcTotalPrice();
+  };
+
+  incrementHandler = id => {
+    const { products } = this.state;
+    const productIndex = products.findIndex(product => product.id === id);
+    if (
+      products[productIndex].quantity < products[productIndex].stockQuantity
+    ) {
+      products[productIndex].quantity += 1;
+    }
+    this.setState({ products: products });
+    this.calcTotalPrice();
+  };
 
   calcTotalPrice = () => {
-    const sum = 1000;
-    // Calclate Price
+    let sum = 0;
+    const { products } = this.state;
+    for (let i = 0; i < products.length; i++) {
+      sum = sum + products[i].price * products[i].quantity;
+    }
     this.setState({ totalPrice: sum });
   };
-  checkoutHandler = () => {};
-  decHandler = () => {};
-  incHandler = () => {};
 
   render() {
     const renderProductList = (
       <FlatList
         data={this.state.products}
-        renderItem={({ item }) => {
-          return (
-            <View style={styles.productCard}>
-              <View style={styles.productImageStyle}>
-                <Image
-                  source={require('../../assets/car.png')}
-                  style={{
-                    width: 100,
-                    height: 100,
-                    borderColor: '#000',
-                    borderWidth: 1
-                  }}
-                />
-              </View>
-              <View style={styles.productInfoStyle}>
-                <Text style={{ fontWeight: '500', fontSize: 15 }}>
-                  Product: {item.productName}
-                </Text>
-                <Text style={{ fontSize: 14, color: '#1e90ff', marginTop: 7 }}>
-                  Price: ${item.price}
-                </Text>
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-around'
-                  }}
+        extraData={this.state}
+        keyExtractor={(item, index) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.productCard} id={item.id}>
+            <View style={styles.productImageHolderStyle}>
+              <Image
+                source={require('../../assets/car.png')}
+                style={styles.productImageStyle}
+              />
+            </View>
+            <View style={styles.productInfoHolderStyle}>
+              <Text style={{ fontWeight: '500', fontSize: 15 }}>
+                {item.productName}
+              </Text>
+              <Text style={{ fontSize: 14, color: '#1e90ff', marginTop: 7 }}>
+                Price: ${item.price}
+              </Text>
+              <View style={styles.qtyControlStyle}>
+                <Text style={{ fontSize: 15, marginTop: 13 }}>Qty: </Text>
+                <TouchableOpacity
+                  style={styles.qtyButton}
+                  onPress={() => this.decrementHandler(item.id)}
                 >
-                  <Text style={{ fontSize: 15, marginTop: 13 }}>Qty:</Text>
-                  <TouchableOpacity
-                    style={styles.button}
-                    onPress={this.decHandler}
-                  >
-                    <Text>-</Text>
-                  </TouchableOpacity>
-                  <Text style={{ fontSize: 15, marginTop: 13 }}>
-                    {item.quantity}
-                  </Text>
-                  <TouchableOpacity
-                    style={styles.button}
-                    onPress={this.incHandler}
-                  >
-                    <Text>+</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-              <View style={styles.productControlStyle}>
-                <Button
-                  onPress={this.checkoutHandler}
-                  title='Remove'
-                  color='#DF423A'
-                />
-                <View />
+                  <Text style={{ fontFamily: 'monospace' }}>-</Text>
+                </TouchableOpacity>
+                <Text style={{ fontSize: 15, marginTop: 13 }}>
+                  {' ' + item.quantity + ' '}
+                </Text>
+                <TouchableOpacity
+                  style={styles.qtyButton}
+                  onPress={() => this.incrementHandler(item.id)}
+                >
+                  <Text style={{ fontFamily: 'monospace' }}>+</Text>
+                </TouchableOpacity>
               </View>
             </View>
-          );
-        }}
+            <View style={styles.productRemoveHolderStyle}>
+              <Button
+                onPress={() => this.removeHandler(item.id)}
+                title='Remove'
+                color='#DF423A'
+              />
+              <View />
+            </View>
+          </View>
+        )}
       />
     );
 
@@ -122,7 +190,9 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     backgroundColor: '#f0f1f7'
   },
-  productsContainer: {},
+  productsContainer: {
+    // backgroundColor: '#ff0'
+  },
   productCard: {
     padding: 15,
     paddingRight: 5,
@@ -132,27 +202,41 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-start'
   },
+  productImageHolderStyle: {
+    marginRight: 20
+  },
   productImageStyle: {
-    marginRight: 20
+    width: 100,
+    height: 100,
+    borderColor: '#000',
+    borderWidth: 1
   },
-  productInfoStyle: {
-    marginRight: 20
+  productInfoHolderStyle: {
+    marginRight: 20,
+    marginLeft: 4
   },
-  productControlStyle: {
-    marginLeft: 20,
+  qtyControlStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  productRemoveHolderStyle: {
+    // backgroundColor: '#555',
+    marginLeft: 40,
     padding: 10,
     justifyContent: 'flex-start'
   },
+  qtyButton: {
+    paddingLeft: 6,
+    paddingRight: 6,
+    marginTop: 13,
+    borderRadius: 5,
+    borderWidth: 2
+  },
   checkoutContainer: {
     justifyContent: 'center'
+    // backgroundColor: '#f0f'
   },
-  button: {
-    paddingLeft: 5,
-    paddingRight: 5,
-    marginTop: 13,
-    borderRadius: 20,
-    borderWidth: 1
-  },
+
   buttonStyle: {
     marginBottom: height / 10,
     paddingLeft: width / 20,
